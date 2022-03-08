657bb3541ab4abb6d5498258d89e691d6df7d940
jeonsumin <soomis7@naver.com>
2022-03-08 21:10:20 +0900

    Initial Commit

---
+++ Cafe/CafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CafeApp.swift
+//  Cafe
+//
+//  Created by Terry on 2022/03/08.
+//
+
+import SwiftUI
+
+@main
+struct CafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
